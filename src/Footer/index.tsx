7ae20763028bbd104fd6f18201d9efc118567329
Alex Lirio <alexlirioti@gmail.com>
2021-01-13 01:28:38 -0300

    hotfix double quotes
    hotfix double quotes

--- src/Footer/index.tsx
+++ src/Footer/index.tsx
@@ -7,16 +7,16 @@ import { ReactComponent as InstagramIcon } from './instagram.svg';
 
 function Footer() {
     return (
-        <footer className='main-footer'>
+        <footer className="main-footer">
             App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
-            <div className='footer-icons'>
-                <a href="https://www.youtube.com/c/DevSuperior" target='_new'>
+            <div className="footer-icons">
+                <a href="https://www.youtube.com/c/DevSuperior" target="_new">
                     <YoutubeIcon />
                 </a>
-                <a href="https://www.linkedin.com/school/devsuperior" target='_new'>
+                <a href="https://www.linkedin.com/school/devsuperior" target="_new">
                     <LinkedinIcon />
                 </a>
-                <a href="https://www.instagram.com/devsuperior.ig" target='_new'>
+                <a href="https://www.instagram.com/devsuperior.ig" target="_new">
                     <InstagramIcon />
                 </a>
             </div>
--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -6,21 +6,21 @@ import { Link } from 'react-router-dom';
 function Home() {
     return (
         <>
-            <div className='home-container'>
-                <div className='home-content'>
-                    <div className='home-actions'>
-                        <h1 className='home-title'>
+            <div className="home-container">
+                <div className="home-content">
+                    <div className="home-actions">
+                        <h1 className="home-title">
                             Faça seu pedido <br /> que entregamos <br /> pra você!!!
                         </h1>
-                        <h3 className='home-subtitle'>
+                        <h3 className="home-subtitle">
                             Escolha o seu pedido e em poucos minutos <br />
                             levaremos na sua porta
                         </h3>
-                        <Link to="/orders" className='home-btn-order'>
+                        <Link to="/orders" className="home-btn-order">
                             FAZER PEDIDO
                         </Link>
                     </div>
-                    <div className='home-image'>
+                    <div className="home-image">
                         <MainImage />
                     </div>
                 </div>
