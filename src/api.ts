ccaacd466f250de8d12ae8bbd9dc395e27944be7
Alex Lirio <alexlirioti@gmail.com>
2021-01-14 00:52:22 -0300

    Add env API_URL and redirect file to netlify
    Add env API_URL and redirect file to netlify

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { OrderPayload } from "./Orders/types";
 
-const API_URL = 'http://127.0.0.1:8081/dsdeliver';
+const API_URL = `${process.env.REACT_APP_API_URL}/dsdeliver`;
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts() {
